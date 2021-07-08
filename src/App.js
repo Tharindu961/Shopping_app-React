c02b033e1edfd306737f44a7f5fa88fd9b466f8b
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-07-08 20:29:07 +0530

    Added Product page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/pages/home";
+import Products from "./components/pages/products";
+
 
 function App() {
   return (
     <Router>
     <Route exact path="/" component={Home}></Route>
+      <Switch>
+          <Route exact path="/products" component={Products}></Route>
+        </Switch>
     </Router>
   );
 }
