75d638206fe409111acefec73d65fd1b05589a3d
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-07-15 00:11:53 +0530

    Account page Enhancement

--- src/components/pages/account.js
+++ src/components/pages/account.js
@@ -40,9 +40,42 @@ function account() {
                     </div>
 
                     <div class="col-2">
+                        <div class="row">
+                            <div class="col-1" style={{marginRight: "300px"}}>
                         <div class="form-container">
+                            <div class="form-btn">
+                                <span>Register</span>
                             
+                                <hr id="Indicator" />
+                            </div>
+
+                            <form>
+                                <input type="text" placeholder="Username" />
+                                <input type="email" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                                <button type="submit" class="btn">Register</button>
+                            </form>
+
                         </div>
+                        </div>
+                        <div class="col-1">
+                        <div class="form-container">
+                            <div class="form-btn">
+                                <span>Login</span>
+                            
+                                <hr id="Indicator" />
+                            </div>
+                            
+                            <form >
+                                <input type="text" placeholder="Username" />
+                                <input type="password" placeholder="Password" />
+                                <button type="submit" class="btn">Login</button>
+                                <a href="">Forgot Password</a>
+                            </form>
+                            </div>
+                
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div>
